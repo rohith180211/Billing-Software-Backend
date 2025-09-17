54e9974e6a7674bb64d0eeaa55792367a271794c
Rohith Janwadkar <rohithjanwadkar@Rohiths-MacBook-Air.local>
2025-09-17 14:32:10 -0500

    Added spring security

--- src/main/java/com/example/billingsoftware/io/CategoryRequest.java
+++ src/main/java/com/example/billingsoftware/io/CategoryRequest.java
@@ -1,11 +1,15 @@
 package com.example.billingsoftware.io;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryRequest {
 
     private String name;
